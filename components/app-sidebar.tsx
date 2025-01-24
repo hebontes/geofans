ba5a2df25585b4ac581fdcc32c7450f9defc11c9
gukan <gukanozadze@gmail.com>
2025-01-25 00:12:15 +0400

    sidebar updates

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -29,8 +29,8 @@ import {
 // This is sample data.
 const data = {
   user: {
-    name: "shadcn",
-    email: "m@example.com",
+    name: "hebontes",
+    email: "hebontes@outlooc.com",
     avatar: "/avatars/shadcn.jpg",
   },
   teams: [
