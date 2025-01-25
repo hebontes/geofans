a0a7130a7c96e34a2f08727c87d7054aa02035fe
gukan <gukanozadze@gmail.com>
2025-01-26 01:48:27 +0400

    Store Detailed layout

--- actions/createUserAction.ts
+++ actions/createUserAction.ts
@@ -1,24 +1,35 @@
 "use server"
 
 import {prisma} from "@/lib/prisma";
+import {cookies} from "next/headers";
+import {SignJWT} from "jose";
 
-const sleep = (milliseconds: number) =>{
+const sleep = (milliseconds: number) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
-export default async function createUserAction(prevState:{message:string},formData: FormData) {
+const secretKey = 'secret'
+const key = new TextEncoder().encode(secretKey)
+
+export async function encrypt(payload: any) {
+    return await new SignJWT(payload)
+        .setProtectedHeader({alg: 'HS256'})
+        .setIssuedAt()
+        .setExpirationTime('10 sec from now')
+        .sign(key)
+
+}
+
+export default async function createUserAction(prevState: { message: string }, formData: FormData) {
     const email = formData.get("email") as string;
     const username = formData.get("username") as string;
 
-    // do something with Prisma:
-    // Prisma Create user
-    // await prisma.user.create({ data: { name:username, username,email } });
-
     // Prisma get user
-        await sleep(2000)
-        const data = await prisma.user.findFirst({ where: { username } });
-        console.log(data)
-        if(!data) return {message: `User not found`}
-        return {message: `Found User ${data.username}`}
+    await sleep(2000)
+    const data = await prisma.user.findFirst({where: {username}});
+    if (!data) return {message: `User not found`}
 
-
+    const expires = new Date(Date.now() + 10 * 1000)
+    const session = await encrypt({data, expires})
+    cookies().set('session', session, {expires, httpOnly: true})
+    return {message: `Found User ${data.username}`}
 }
